b9ce1c26c30d01962d4b321806d5a7a06a9ec781
xiaobai <106022124+SHAORANBAI@users.noreply.github.com>
2022-07-20 19:25:34 +0900

    Add files via upload

---
+++ 0705study/0705study/0705.C
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+	/*int i = 1;
+	while (i <= 10)
+	{
+		printf("hello world %d\n", i++);
+	}*/
+
+	//for (int i = 1; i <= 3; i++)
+	//{
+	//	printf(" 첫 번째 반복문 : %d\n", j)
+
+	//		for (int j = 1; j <= 5; j++) {
+	//			printf("두 번째 반복문 : %d\n", j);
+	//		}
+
+	//}
+
+	//return 0;
+
+	//for (int i = 2; i <= 9; i++)
+	//{
+	//	for (int j = 1; j <= 9; j++)
+	//	{
+	//		printf(" %d x %d = %d\n", i, j, i * j);
+	//	}
+	//}
+
+	//for (int i = 0 ; i < 5 i++);
+	//{
+	//	for (int j = 0; j <= i; j++)
+	//	{
+	//		printf("*")
+	//	}
+	//	printf("*\n")
+	//}
+
+
+}
